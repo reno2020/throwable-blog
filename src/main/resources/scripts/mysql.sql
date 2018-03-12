a27af07239bad2481b7f39a1161ad8528cc2d19e
zhangjc <zhangjinci@ppmoney.com>
2018-03-12 22:07:38 +0800

    feat:添加link表

--- src/main/resources/scripts/mysql.sql
+++ src/main/resources/scripts/mysql.sql
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS `t_category_count` (
   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
   category_id BIGINT UNSIGNED COMMENT '类型id',
   category_name VARCHAR(30) NOT NULL COMMENT '类型名称',
-  total_count INT UNSIGNED DEFAULT '0' COMMENT '数量'
+  total_count INT UNSIGNED DEFAULT '0' COMMENT '数量',
+  INDEX idx_category_id(category_id)
 ) COMMENT '文章类型数量统计表';
 
 CREATE TABLE IF NOT EXISTS `t_article` (
@@ -21,7 +22,11 @@ CREATE TABLE IF NOT EXISTS `t_article` (
   create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   views INT COMMENT '浏览数量',
-  replies INT COMMENT '回复数量'
+  replies INT COMMENT '回复数量',
+  INDEX idx_title(title),
+  INDEX idx_category_id(category_id),
+  INDEX idx_create_time(create_time),
+  INDEX idx_update_time(update_time)
 )COMMENT '文章表';
 
 CREATE TABLE IF NOT EXISTS `t_article_content`(
@@ -30,3 +35,13 @@ CREATE TABLE IF NOT EXISTS `t_article_content`(
   content LONGTEXT COMMENT '文章内容',
   UNIQUE unique_article_id(article_id)
 )COMMENT '文章内容表';
+
+CREATE TABLE IF NOT EXISTS `t_link`(
+  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
+  title VARCHAR(30) COMMENT '名称',
+  url VARCHAR(50) COMMENT 'url',
+  sort TINYINT COMMENT '排序参数',
+  create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  is_delete TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除'
+)COMMENT '首页链接';
